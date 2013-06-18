866e3e7177766c8e24225109f20927a9cafe3348
Jaime Pillora <jpillora@gmail.com>
2013-06-18 17:53:20 +1000

    cleaned up numeric rules

--- dist/verify.js
+++ dist/verify.js
@@ -1962,11 +1962,18 @@ log("plugin added.");
      * Ensures valid currency
      * @name currency
      * @type field
+     * @param symbol The currency symbol to expect
+     * 
+     * @valid $100.00
+     * @valid -$100.00
+     * @valid #params(*) -*100.00
+     * @invalid 100.00
+     * @invalid -*100.00
      */
     currency: {
       fn: function(r) {
         if(r.args[0]) r.symbol = r.args[0];
-        if(!/^\-?(.)\d{1,2}(,?\d{3})*(\.\d+)?$/.test(r.val()))
+        if(!/^\-?(.)\d+(,?\d{3})*(\.\d+)?$/.test(r.val()))
           return r.message.invalidValue;
         if(!RegExp.$1 || RegExp.$1 !== r.symbol)
           return r.message.invalidCurrency;
@@ -1976,50 +1983,99 @@ log("plugin added.");
       symbol: '$',
       message: {
         invalidValue: "Invalid monetary value",
-        invalidCurrency: "Invalid Currency {{ symbol }}"
+        invalidCurrency: "Missing '{{ symbol }}' symbol"
       }
     },
-    decimal: function(r) {
-      var vStr = r.val(),
-          places = r.args[0] ? parseInt(r.args[0], 10) : 2;
+    /**
+     * Ensures valid decimal number and rounds it
+     * @name decimal
+     * @type field
+     * @param places The numbers of places to round
+     */
+    decimal: {
+      fn: function(r) {
+        var vStr = r.val(),
+            places = r.args[0] ? parseInt(r.args[0], 10) : 2;
 
-      if(!vStr.match(/^\-?\d+(,\d{3})*(\.\d+)?$/))
-        return "Invalid decimal value";
+        if(!vStr.match(/^\-?\d+(,\d{3})*(\.\d+)?$/))
+          return r.message;
 
-      var v = parseFloat(vStr.replace(/[^\d\.]/g,'')),
-          factor = Math.pow(10,places);
+        var v = parseFloat(vStr.replace(/[^\d\.]/g,'')),
+            factor = Math.pow(10,places);
 
-      v = (Math.round(v*factor)/factor);
-      r.field.val(v);
+        v = (Math.round(v*factor)/factor);
+        r.field.val(v);
 
-      return true;
+        return true;
+      },
+      message: "Invalid decimal value"
     },
-    minVal: function(r) {
-      var v = parseFloat(r.val().replace(/[^\d\.]/g,'')),
-          suffix = r.args[1] || '',
-          min = parseFloat(r.args[0]);
-      if(v < min)
-        return "Must be greater than " + min + suffix;
-      return true;
+    /**
+     * Ensures the input is greater than the given value
+     * @name minVal
+     * @type field
+     * @param min The minimum value
+     */
+    minVal: {
+      fn:function(r) {
+        var v = parseFloat(r.val().replace(/[^\d\.]/g,''));
+        r.min = parseFloat(r.args[0]);
+        if(r.args[1]) r.suffix = r.args[1];
+        if(r.args[2]) r.suffix = r.args[2];
+        if(v < r.min)
+          return r.message;
+        r.val(v);
+        return true;
+      },
+      prefix: '',
+      suffix: '',
+      message: "Must be greater than { prefix }{ min }{ suffix }"
     },
-    maxVal: function(r) {
-      var v = parseFloat(r.val().replace(/[^\d\.]/g,'')),
-          suffix = r.args[1] || '',
-          max = parseFloat(r.args[0]);
-      if(v > max)
-        return "Must be less than " + max + suffix;
-      return true;
+    /**
+     * Ensures the input is less than the given value
+     * @name maxVal
+     * @type field
+     * @param max The maximum value
+     */
+    maxVal: {
+      fn:function(r) {
+        var v = parseFloat(r.val().replace(/[^\d\.]/g,''));
+        r.max = parseFloat(r.args[0]);
+        if(r.args[1]) r.suffix = r.args[1];
+        if(r.args[2]) r.suffix = r.args[2];
+        if(v > r.max)
+          return r.message;
+        r.val(v);
+        return true;
+      },
+      prefix: '',
+      suffix: '',
+      message: "Must be less than { prefix }{ max }{ suffix }"
     },
-    rangeVal: function(r) {
-      var v = parseFloat(r.val().replace(/[^\d\.]/g,'')),
-          prefix = r.args[2] || '',
-          suffix = r.args[3] || '',
-          min = parseFloat(r.args[0]),
-          max = parseFloat(r.args[1]);
-      if(v > max || v < min)
-        return "Must be between " + prefix + min + suffix + "\nand " + prefix + max + suffix;
-      return true;
+    /**
+     * Ensures the input is within the given range
+     * @name rangeVal
+     * @type field
+     * @param min The minimum value
+     * @param max The maximum value
+     */
+    rangeVal: {
+      fn: function(r) {
+        var v = parseFloat(r.val().replace(/[^\d\.]/g,''));
+
+        r.min = parseFloat(r.args[0]);
+        r.max = parseFloat(r.args[1]);
+        if(r.args[2]) r.suffix = r.args[2];
+        if(r.args[3]) r.suffix = r.args[3];
+
+        if(v > r.max || v < r.min)
+          return r.message;
+        return true;
+      },
+      prefix: '',
+      suffix: '',
+      message: "Must be between { prefix }{ min }{ suffix }\nand { prefix }{ max }{ suffix }"
     }
-
+    /** */
   });
 })(jQuery);}(window,document,jQuery));
