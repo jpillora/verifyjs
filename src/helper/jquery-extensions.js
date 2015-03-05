a940308341bd692744ca046ef3239f2956e8fe13
Zhang zhengzheng <code@tychio.net>
2015-03-05 17:12:33 +0800

    [fix]no jquery conflict for some extensions
    In fact, I am using Prototype($) and jQuery with VerifyJS, so that I always got some message that verifyScrollView is undefined.

--- src/helper/jquery-extensions.js
+++ src/helper/jquery-extensions.js
@@ -2,37 +2,37 @@
 /* ===================================== *
  * jQuery Extensions
  * ===================================== */
-
-$.fn.verifyScrollView = function(onComplete) {
-  var field = $(this).first();
-  if(field.length !== 1) return $(this);
-  return $(this).verifyScrollTo(field, onComplete);
-};
-
-$.fn.verifyScrollTo = function( target, options, callback ){
-  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
-  var settings = $.extend({
-    scrollTarget  : target,
-    offsetTop     : 50,
-    duration      : 500,
-    easing        : 'swing'
-  }, options);
-  return this.each(function(){
-    var scrollPane = $(this);
-    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
-    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop, 10);
-    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration, 10), settings.easing, function(){
-      if (typeof callback == 'function') { callback.call(this); }
+(function ($) {
+  $.fn.verifyScrollView = function(onComplete) {
+    var field = $(this).first();
+    if(field.length !== 1) return $(this);
+    return $(this).verifyScrollTo(field, onComplete);
+  };
+  
+  $.fn.verifyScrollTo = function( target, options, callback ){
+    if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+    var settings = $.extend({
+      scrollTarget  : target,
+      offsetTop     : 50,
+      duration      : 500,
+      easing        : 'swing'
+    }, options);
+    return this.each(function(){
+      var scrollPane = $(this);
+      var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+      var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop, 10);
+      scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration, 10), settings.easing, function(){
+        if (typeof callback == 'function') { callback.call(this); }
+      });
     });
-  });
-};
-
-$.fn.equals = function(that) {
-  if($(this).length !== that.length)
-    return false;
-  for(var i=0,l=$(this).length;i<l;++i)
-    if($(this)[i] !== that[i])
+  };
+  
+  $.fn.equals = function(that) {
+    if($(this).length !== that.length)
       return false;
-  return true;
-};
-
+    for(var i=0,l=$(this).length;i<l;++i)
+      if($(this)[i] !== that[i])
+        return false;
+    return true;
+  };
+})(jQuery);
